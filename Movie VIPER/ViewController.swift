249e23584bd5f224871dde39633062661b255a60
Gremlinflat <fahri.novaldi91100@gmail.com>
2022-09-22 20:20:34 +0700

    Initial Commit

---
+++ Movie VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie VIPER
+//
+//  Created by Fahri Novaldi on 22/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
