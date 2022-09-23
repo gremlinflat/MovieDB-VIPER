3ea8b811919bd487e5a9e48eea7834c59c65bf6a
Gremlinflat <fahri.novaldi91100@gmail.com>
2022-09-23 09:12:31 +0700

    chore(networkError)

--- Movie VIPER/Services/NetworkManager.swift
+++ Movie VIPER/Services/NetworkManager.swift
@@ -30,12 +30,8 @@ class NetworkManager: NSObject {
 enum NetworkError: String, Error {
     case missingUrl = "URL is missing"
     case decodingFailed = "Decoding failed"
-    case authenticationError = "You need to be authenticated first or you not in Academy"
     case badRequest = "Bad request"
-    case outdated = "The url you requested is outdated."
     case failed = "Network request failed."
     case noData = "Response returned with no data to decode."
     case unableToDecode = "We could not decode the response."
-    case userExist = "User already exist"
-    case expiredToken = "Expired Token"
 }
